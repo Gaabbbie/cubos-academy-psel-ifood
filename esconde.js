e6697362f5f0a9b53d8e7ef61190c1bcde179324
Gabriel23230 <gabrielsts18@hotmail.com>
2021-08-11 16:54:37 -0300

    Brasilsilsil

---
+++ esconde.js
@@ -0,0 +1,11 @@
+function solucao(primeiraLetra, segundaLetra, palavras) {
+    let i = 21
+    if (numero <= 20) {
+      console.log('VOCE CONSEGUE, MANINHO')
+    } else if (numero >= 21) {
+      for (i; i <= numero; i++) {
+        console.log(i)
+      }
+    }
+
+}
