9ddfa831b73972f14f65e34ddbd6d3326d0e9591
Gabriel23230 <gabrielsts18@hotmail.com>
2021-08-11 17:09:33 -0300

    Brasilsilsil

--- brasil.js
+++ brasil.js
@@ -1,11 +1,20 @@
 function solucao(primeiraLetra, segundaLetra, palavras) {
-    let i = 21
-    if (numero <= 20) {
-      console.log('VOCE CONSEGUE, MANINHO')
-    } else if (numero >= 21) {
-      for (i; i <= numero; i++) {
-        console.log(i)
-      }
+	var palavras = ["aveia", "manha", "ave"];
+
+function solucao(primeiraLetra, segundaLetra, palavras) {
+  let resultado = []
+  for (let item of palavras) {
+    if (item[0] === primeiraLetra && item[1] === segundaLetra) {
+      resultado.push(item)
+      console.log(item)
     }
 
+  }
+  if (resultado.length === 0) {
+    console.log('NENHUMA')
+  }
+}
+
+
+solucao('a', 'v', palavras)
 }
